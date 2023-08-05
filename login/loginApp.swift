40ef9750733deb8d91140c276e44e485ab7f8fdb
phong070 <tpcreative.co@gmail.com>
2023-08-05 13:17:47 +0700

    Initial Commit

---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by phong070 on 05/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
